7a745572e71446760ed32f151183d13c0bd8e405
Amulya <karlamulya341@gmail.com>
2025-10-12 11:47:10 +0530

    Exploring Routes

--- src/app.js
+++ src/app.js
@@ -1,17 +1,31 @@
 const express = require('express');
 const app = express();
 
-app.use("/home",(req, res)=>{
-    res.send("This is home page");
+// app.use("/home",(req, res)=>{
+//     res.send("This is home page");
+// });
+
+// app.use("/hello", (req, res)=>{
+//     res.send("This is hello page")
+// })
+
+// app.use("/", (req, res)=>{
+//     res.send("This is start page")
+// })
+
+app.get("/user", (req,res)=>{
+    res.send("got the data")
 });
 
-app.use("/hello", (req, res)=>{
-    res.send("This is hello page")
-})
-
-app.use("/", (req, res)=>{
-    res.send("This is start page")
-})
+app.post("/user", (req,res)=>{
+    res.send("post the data")    
+});
+app.delete("/user", (req,res)=>{
+    res.send("delete the data");    
+});
+app.put("/user", (req,res)=>{
+    res.send("put the data");  
+});
 
 app.listen(7777, ()=>{
     console.log('sucessful iwth 7777')
