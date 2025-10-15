cec50527ef9e9dc57e8980930b3829cb40a82800
Amulya <karlamulya341@gmail.com>
2025-10-15 21:50:14 +0530

    multiple route handlers

--- src/app.js
+++ src/app.js
@@ -13,19 +13,25 @@ const app = express();
 //     res.send("This is start page")
 // })
 
-app.get("/user", (req,res)=>{
-    res.send("got the data")
-});
+// app.get("/user", (req,res)=>{
+//     res.send("got the data")
+// });
 
-app.post("/user", (req,res)=>{
-    res.send("post the data")    
-});
-app.delete("/user", (req,res)=>{
-    res.send("delete the data");    
-});
-app.put("/user", (req,res)=>{
-    res.send("put the data");  
-});
+app.use("/hello", 
+    (req, res, next)=>{
+    res.send("This is hello page1");
+    next();
+},
+(req, res, next)=>{
+    res.send("This is hello page 2");
+    next();
+},
+(req, res, next)=>{
+    res.send("This is hello page 3");
+    next();
+},
+)
+
 
 app.listen(7777, ()=>{
     console.log('sucessful iwth 7777')
