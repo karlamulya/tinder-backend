172ea08504c9fc4ce55e3aa05a14eb11478148f9
Amulya <karlamulya341@gmail.com>
2025-12-08 23:47:27 +0530

    added ptach and delte apis

--- src/app.js
+++ src/app.js
@@ -32,6 +32,27 @@ app.get("/user", async(req, res)=>{
   }
 });
 
+app.delete("/user", async(req, res)=>{
+    const userId = req.body.userId;
+  try{
+    const user =  await User.findByIdAndDelete(userId);
+      res.send("User deleted successfully");
+  }catch{
+      res.status(400).send("something went wrong");
+  }
+});
+
+app.patch("/user", async(req, res)=>{
+   const userId= req.body.userId;
+   const data = req.body;
+   try{
+      await User.findByIdAndUpdate({_id: userId}, data);
+      res.send("User updated successfully")
+   }catch{
+    res.status(400).send("something went wrong");
+   }
+})
+
 app.get("/feed", async(req, res)=>{
   try{
       const user = await User.find({});
