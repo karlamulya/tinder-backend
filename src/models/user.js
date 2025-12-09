b29c9707ba9666ac6ba50ced305bb1d28188e35c
Amulya <karlamulya341@gmail.com>
2025-12-10 00:06:16 +0530

    validations

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const userScheme = new mongoose.Schema({
     firstName:{
@@ -13,7 +14,12 @@ const userScheme = new mongoose.Schema({
         required:true,
         unique: true,
         lowercase: true,
-        trim:true
+        trim:true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error("Email is not valid")
+            }
+        }
     },
     mobile:{
         type: Number
